45efbe0e76f624a82634c3cbe18cc05bddba4a91
DSchirmer1987 <dschirmer1987@gmail.com>
2022-11-15 09:16:11 +0100

    Beispiel Fehler 1 und 2

--- Grundstruktur_Dozent/src/grundstruktur_dozent/App.java
+++ Grundstruktur_Dozent/src/grundstruktur_dozent/App.java
@@ -3,8 +3,15 @@ package grundstruktur_dozent;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String text = "Hallo Welt";
+		int zahl = 0;
+		
+//		int zahlText = Integer.valueOf(text);
+		
+		int a = 5;
+		int b = 10;
+		
+		System.out.println("b " + a + " + a " + b + " = 15 " + (a+b));
 	}
 
 }
